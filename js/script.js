fa64e69a26566b0d6bccf96afc1ab2077cfa2b67
Kristen Stabile <kristenlstabile@gmail.com>
2018-06-27 12:48:14 -0400

    second commit

--- js/script.js
+++ js/script.js
@@ -1,7 +1,39 @@
-/* ==========================================
+/* ============================================
    Hide Preloader When Whole Site is Loaded
 ========================================== */
 $(window).on('load', function () {
     $('#status').fadeOut();
     $('#preloader').delay(350).fadeOut('slow');
 });
+
+
+
+/* ============================================
+      Team Section - Owl Carousel 2-2.2.1
+============================================ */
+$(function() {
+    $("#team-right").owlCarousel({
+        items: 2,
+        autoplay: true,
+        smartSpeed: 700,
+        loop: true,
+        autoplayHoverPause: true
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
